982c21b17810402c0e5345a48c5f83841824b1b7
m-ally8 <mnenga8@gmail.com>
2026-02-11 20:34:48 +0300

    Added URL names in app urls file

--- APPOINTMENTS/urls.py
+++ APPOINTMENTS/urls.py
@@ -5,12 +5,12 @@ from .views import *
 
 urlpatterns = [
  
-    path('doctors/list/', DoctorListView.as_view()),
-    path('doctors/availability/', DoctorAvailabilityView.as_view() ),
-    path('appointments/list/', AppointmentListView.as_view()),
-    path('appointments/delete/<int:id>/', AppointmentDeleteView.as_view()),
-    path('appointments/details/<int:id>/', AppointmentDetailsView.as_view()),
-    path('appointments/create/', AppointmentCreateView.as_view()),
-    path('appointments/update/<int:id>/', AppointmentUpdateView.as_view()),
+    path('doctors/list/', DoctorListView.as_view(), name="doctors_list"),
+    path('doctors/availability/', DoctorAvailabilityView.as_view(), name="doctors_availability" ),
+    path('appointments/list/', AppointmentListView.as_view(), name="appointments_list"),
+    path('appointments/delete/<int:id>/', AppointmentDeleteView.as_view(), name="appointments_delete"),
+    path('appointments/details/<int:id>/', AppointmentDetailsView.as_view(), name="appointments_details"),
+    path('appointments/create/', AppointmentCreateView.as_view(), name="appointments_create"),
+    path('appointments/update/<int:id>/', AppointmentUpdateView.as_view(), name="appointments_update"),
     
 ]
